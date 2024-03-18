e5f6ae328764c7ebed146a55fa7c9f024b3569b4
Sabrine Barreto <sabrine0barreto@gmail.com>
2024-03-18 13:51:14 -0300

    Seller entity class

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,6 +1,9 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Program {
 
@@ -9,6 +12,10 @@ public class Program {
 		Department obj = new Department (1, "books");
 		
 		System.out.println(obj);
+		
+		Seller sel = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, obj);
+		
+		System.out.println(sel);
 	}
 
 }
